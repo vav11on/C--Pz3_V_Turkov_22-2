75b008758df5b57f689273b3b85ade5d706a8350
Vsevolod <vsevolodturkov8@gmail.com>
2022-11-22 10:08:39 +0200

    1 Запустити програму, що наведено нижче.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string names[4] = {"Mariya", "Pavlo", "Dmytro", "Olga"};
+    cout << names[0] << endl;
+    cout << names[3] << endl;
+    return 0;
+}
