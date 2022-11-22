b2622b65b4ccae52be67f71c59d4e0a02bda3d38
Vsevolod <vsevolodturkov8@gmail.com>
2022-11-22 10:27:33 +0200

    1.2 Змінити другий елемент масиву на своє ім'я.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,13 @@
 #include <string>
 using namespace std;
 int main() {
-    string names[4] = {"Mariya", "Pavlo", "Dmytro", "Olga"};
+    string names[4] = {"Mariya", "Pavlo", "Dmytro", "Vsevolod"};
     cout << names[0] << endl;
     cout << names[3] << endl;
     return 0;
 }
 "E:\Projects C++\C--Pz3_V_Turkov_22-2\cmake-build-debug\C__Pz3_V_Turkov_22_2.exe"
 Mariya
-        Olga
+        Vsevolod
 
 Process finished with exit code 0
-
